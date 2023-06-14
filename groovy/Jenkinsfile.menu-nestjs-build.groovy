3315974514b1d06e90b22e0f9bed902122a79acd
Stanislau Shliakhtsich <stasgm@gmail.com>
2023-06-14 18:22:46 +0300

    Update

--- groovy/Jenkinsfile.menu-nestjs-build.groovy
+++ groovy/Jenkinsfile.menu-nestjs-build.groovy
@@ -24,7 +24,8 @@ pipeline {
 		stage('Login') {
 			steps {
 				docker.withRegistry( '', 'dockerhub-cred' ) {
-				dockerImage.push()
+					dockerImage.push()
+				}
 				// sh 'echo $DOCKERHUB_CREDENTIALS_PSW | docker login -u $DOCKERHUB_CREDENTIALS_USR --password-stdin'
 			}
 		}
