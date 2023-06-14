e0461ddcf51264160769fe9c4b3cf79c2cb678a2
Stanislau Shliakhtsich <stasgm@gmail.com>
2023-06-14 17:55:53 +0300

    Update groovy file

---
+++ groovy/Jenkinsfile.menu-nestjs-build.groovy
@@ -0,0 +1,41 @@
+pipeline {
+	agent any
+	options {
+		skipDefaultCheckout(true)
+		buildDiscarder(logRotator(numToKeepStr: '5'))
+	}
+	environment {
+		DOCKERHUB_CREDENTIALS = credentials('dockerhub-cred')
+	}
+	stages {
+		stage('Cloning git repo') {
+			steps {
+					cleanWs()
+					git url: 'https://github.com/stasgm/nestjs-menu.git'
+			}
+		}
+		stage('Build') {
+			steps{
+				sh 'docker build . -t stanislau2020/nestjs-menu:latest'
+			}
+		}
+		stage('Login') {
+      steps {
+        sh 'echo $DOCKERHUB_CREDENTIALS_PSW | docker login -u $DOCKERHUB_CREDENTIALS_USR --password-stdin'
+      }
+    }
+		stage('Push'){
+			steps{
+				sh 'docker push stanislau2020/nestjs-menu:latest'
+				// withCredentials([usernamePassword(credentialsId: 'docker_token', passwordVariable: 'dockerHubPassword', usernameVariable: 'dockerHubUser')]) {
+				// sh "docker login -u ${env.dockerHubUser} -p ${env.dockerHubPassword}"
+				// }
+			}
+		}
+		post {
+			always {
+				sh 'docker logout'
+			}
+		}
+	}
+}
